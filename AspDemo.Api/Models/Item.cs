e8154f5ab11145b02748efeb24240909287a65c6
TuralQuliyev2124 <tguliev9@gmail.com>
2022-03-12 01:31:43 +0400

    Implemented Test Method

--- AspDemo.Api/Models/Item.cs
+++ AspDemo.Api/Models/Item.cs
@@ -4,16 +4,9 @@ namespace AspDemo.Api.Models
 {
     public class Item
     {
-        // private static int idCounter = 0;
         public int Id { get; init; }
         public string? Name { get; init; }
         public decimal Price { get; init; }
         public DateTimeOffset CreatedTime { get; init; }
-
-        // public Item()
-        // {
-        //     Id = ++idCounter;
-        //     CreatedTime = DateTimeOffset.UtcNow;
-        // }
     }
 }
--- AspDemo.UnitTests/ItemsControllerTests.cs
+++ AspDemo.UnitTests/ItemsControllerTests.cs
@@ -4,6 +4,7 @@ using AspDemo.Api.Controllers;
 using AspDemo.Api.DTOs;
 using AspDemo.Api.Models;
 using AspDemo.Api.Repositories;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Xunit;
@@ -12,12 +13,13 @@ namespace AspDemo.UnitTests;
 
 public class ItemsControllerTests
 {
+    private readonly Random rand = new Random();
+    private readonly Mock<IItemsRepository> RepositoryStub = new Mock<IItemsRepository>();
+
     [Fact]
-    public async Task GetItemAsync_WithUexistingItem_ReturnsNotFound()
+    public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
     {
         // Arrange
-        Random rand = new Random();
-        Mock<IItemsRepository> RepositoryStub = new Mock<IItemsRepository>();
         RepositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<int>())).ReturnsAsync((Item)null);
 
         ItemsController controller = new ItemsController(RepositoryStub.Object);
@@ -27,6 +29,35 @@ public class ItemsControllerTests
 
         // Assert
         Assert.IsType<NotFoundResult>(result.Result);
+    }
 
+    [Fact]
+    public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+    {
+        // Arrange 
+        Item expectedItem = CreateRandomItem();
+        RepositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<int>())).ReturnsAsync(expectedItem);
+
+        ItemsController controller = new ItemsController(RepositoryStub.Object);
+
+        // Act 
+        ActionResult<ItemDTO> result = await controller.GetItemAsync(rand.Next());
+
+        // Assert
+        // Problems here need refactoring
+        var okresult = Assert.IsType<OkObjectResult>(result.Result);
+        Assert.IsType<ItemDTO>(okresult.Value);
+        //result.Value.Should().BeEquivalentTo(expectedItem);
+    }
+
+    private Item CreateRandomItem()
+    {
+        return new Item()
+        {
+            Id = rand.Next(),
+            Name = Guid.NewGuid().ToString(),
+            Price = rand.Next(),
+            CreatedTime = DateTimeOffset.UtcNow
+        };
     }
 }
