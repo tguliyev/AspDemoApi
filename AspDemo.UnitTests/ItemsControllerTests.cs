e7490d085b860ecbf5a5522cfe025e94e8b66a46
TuralQuliyev2124 <tguliev9@gmail.com>
2022-03-11 17:01:55 +0400

    Add GetItem Test

--- AspDemo.UnitTests/ItemsControllerTests.cs
+++ AspDemo.UnitTests/ItemsControllerTests.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Threading.Tasks;
+using AspDemo.Api.Controllers;
+using AspDemo.Api.DTOs;
+using AspDemo.Api.Models;
+using AspDemo.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using Xunit;
 
 namespace AspDemo.UnitTests;
@@ -5,12 +13,20 @@ namespace AspDemo.UnitTests;
 public class ItemsControllerTests
 {
     [Fact]
-    public void GetItemAsync_WithUexistingItem_ReturnsNotFound()
+    public async Task GetItemAsync_WithUexistingItem_ReturnsNotFound()
     {
         // Arrange
+        Random rand = new Random();
+        Mock<IItemsRepository> RepositoryStub = new Mock<IItemsRepository>();
+        RepositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<int>())).ReturnsAsync((Item)null);
 
+        ItemsController controller = new ItemsController(RepositoryStub.Object);
+        
         // Act
+        ActionResult<ItemDTO> result = await controller.GetItemAsync(rand.Next());
 
         // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
+
     }
 }
